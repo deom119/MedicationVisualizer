dede0193327068feb595f2536e34e6cfcce52406
Dasom <deom119@gmail.com>
2020-03-05 20:35:51 -0500

    npm run build performed

---
+++ build/precache-manifest.4f3d0becc282881014944fbb7b300687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788621b9f33053f95f7905d78af6facd",
+    "url": "/medviz2-frontend/index.html"
+  },
+  {
+    "revision": "37ce7c205868f8ca9fcf",
+    "url": "/medviz2-frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c5269a9b7d7e6c55c02e",
+    "url": "/medviz2-frontend/static/js/2.2cfc58b7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/medviz2-frontend/static/js/2.2cfc58b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ce7c205868f8ca9fcf",
+    "url": "/medviz2-frontend/static/js/main.51c35475.chunk.js"
+  },
+  {
+    "revision": "8bd0ebed9e616823ec9d",
+    "url": "/medviz2-frontend/static/js/runtime-main.492f7e83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/medviz2-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
