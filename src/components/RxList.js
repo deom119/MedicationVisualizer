b2ada7d52d11b324016fe5a7d886c07e4d9de476
Dasom <deom119@gmail.com>
2020-03-07 18:55:08 -0500

    https back to http

--- src/components/RxList.js
+++ src/components/RxList.js
@@ -24,7 +24,7 @@ const RxList = () => {
 
     useEffect(()=>{
         setIsLoading(true);
-        fetch(`https://hapi.fhir.org/baseR4/Medication`)
+        fetch(`http://hapi.fhir.org/baseR4/Medication`)
             .then(response =>{
                 if(response.ok){
                     return response.json();
