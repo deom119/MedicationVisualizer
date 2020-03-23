565c7c3988d7e85f39fd10193ba74c893c9d910d
Dasom <deom119@gmail.com>
2020-03-23 15:13:26 -0400

    added dropdown option and fixed some errors

--- src/components/RxList.js
+++ src/components/RxList.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const RxList = () => {
 
-    const [rxLists,setRxLists] = useState([])
+    const [rxLists,setRxLists] = useState([]);
     //const [resource,setResource] = useState({})    
     //const [rx,setRx] = useState({})
     //const [code,setCode] = useState("")
@@ -11,18 +12,31 @@ const RxList = () => {
     //const [name,setName] = useState("")
     //const [isRxLoading,setIsRxLoading] =useState(false)
 
-    const [isLoaded,setIsLoaded] =useState(false)
-    const [error,setError]=useState(null)
-    const [searchWord, setsearchWord] = useState("")
+    const [isLoaded,setIsLoaded] =useState(false);
+    const [error,setError]=useState(null);
+    const [searchWord, setsearchWord] = useState("");
+    const [select, setselect] = useState('0');
     const [count, setCount] = useState(0);
+
+    const updateDropdown = (e)=>{
+        setselect(e.target.value)
+    };
 
     const handleSearch = (e)=>{
         setsearchWord(e.target.value)
-    }
+    };
 
-    const displayRx = ()=>{        
-        const url =`https://apps.hdap.gatech.edu/hapiR4/baseR4/Medication?code:text=`+searchWord+`&_pretty=true`;
-        //const url =`https://apps.hdap.gatech.edu/hapiR4/baseR4/Medication?code:text=tylenol&_pretty=true`\
+
+    const displayRx = ()=>{
+        console.log(select);
+        var url;
+            if (select == '0') {
+            url =`https://apps.hdap.gatech.edu/hapiR4/baseR4/Medication?code:text=`+searchWord+`&_pretty=true`;
+        } else if (select == '1') {
+            url =`https://apps.hdap.gatech.edu/hapiR4/baseR4/Medication?code=`+searchWord+`&_pretty=true`;
+        } else {
+            url =`https://apps.hdap.gatech.edu/hapiR4/baseR4/Medication?lot-number=`+searchWord+`&_pretty=true`;
+        }
         console.log(url);
         //console.log(searchWord)
         if(searchWord){
@@ -30,43 +44,54 @@ const RxList = () => {
             fetch(url)
             .then(response =>{
                 if(response.ok){
+                    console.log("response ok")
                     return response.json();
                 }else{
                     throw Error ("Error while fetching data")
                 }
             })
             .then(rx=>{
+
+                //console.log("check 1 " + rx.entry.length);
                 const count = rx.total
                 for (var i = 0; i < rx.entry.length; i++) {
+                    //console.log(i + " turn");
                     rx.entry[i].resource.ingredients = '';
+                    //console.log("check 11");
                     if (rx.entry[i].resource.batch === undefined) {
-                        rx.entry[i].resource.batch.expirationDate = "-";
-                        rx.entry[i].resource.batch.lotNumber = "-";
+                        rx.entry[i].resource.batch.expirationDate = '';
+                        rx.entry[i].resource.batch.lotNumber = '';
                     }
+                    //console.log("check 12");
                     if (rx.entry[i].resource.code.coding[0] === undefined) {
-                        rx.entry[i].resource.code.coding[0].code = "-";
-                        rx.entry[i].resource.code.coding[0].display = "-";
+                        rx.entry[i].resource.code.coding[0].code = '';
+                        rx.entry[i].resource.code.coding[0].display = '';
                     }
+                    //console.log("check 13");
                     if (rx.entry[i].resource.form.coding[0] === undefined) {
-                        rx.entry[i].resource.form.coding[0].display = "-";
+                        rx.entry[i].resource.form.coding[0].display = '';
                     }
+                    //console.log("check 14");
                     if (rx.entry[i].resource.ingredient === undefined) {
-                        rx.entry[i].resource.ingredient[0].itemCodeableConcept.coding[0].code = "-";
-                        rx.entry[i].resource.ingredient[0].itemCodeableConcept.coding[0].display = "-";
+                        rx.entry[i].resource.ingredient[0].itemCodeableConcept.coding[0].code = '';
+                        rx.entry[i].resource.ingredient[0].itemCodeableConcept.coding[0].display = '';
                     }
-
+                    //console.log("check 15");
+                    //console.log(rx.entry[i].resource.ingredient.length);
                     for (var j = 0; j < rx.entry[i].resource.ingredient.length; j++) {
-                        rx.entry[i].resource.ingredients += rx.entry[i].resource.ingredient[j].itemCodeableConcept.coding[0].display + " ";
+                        if (rx.entry[i].resource.ingredient[j].itemCodeableConcept !== undefined) {
+                            rx.entry[i].resource.ingredients += rx.entry[i].resource.ingredient[j].itemCodeableConcept.coding[0].display + " ";
+                        }
                     }
 
 
                 }
+
                 if (count> 0){                
                     setCount(count);
                     setRxLists(rx.entry);
                     setIsLoaded(true);
                 }else{
-                    console.log(`no records returned`);
                     setIsLoaded(true)
                 }
                 return rx.entry;
@@ -98,14 +123,21 @@ const RxList = () => {
     return (
         <React.Fragment>
 
-            <div className="search-bar">
-                <div>
-                    <input type="search" placeholder="Search Medication" aria-label="Search" onChange={handleSearch}></input>
-                </div>
-                <div>
-                    <input type="submit" value="Search" onClick={displayRx}></input>
+            <div className="input-group input-group-lg mb-lg-3" role="toolbar" aria-label="Toolbar with button groups">
+                <select onChange={updateDropdown} className="custom-select input-group-prepend col-md-2" id="dropdown">
+                    <option value='0' defaultValue="selected">Name</option>
+                    <option value='1'>Code</option>
+                    <option value='2'>Batch No</option>
+                </select>
+
+                <input className="form-control" type="search" placeholder="Search Medication"  aria-label="Text input with dropdown button" onChange={handleSearch}/>
+
+                <div className="input-group-append">
+                    <input className="btn btn-primary" type="submit" value="Search" onClick={displayRx}>
+                    </input>
                 </div>
             </div>
+
 
             { !isLoaded?(<p>No records to display</p>):(  
                 <React.Fragment>
