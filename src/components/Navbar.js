feb1001826749456db561b33377026b473575538
Dasom <deom119@gmail.com>
2020-03-23 16:14:10 -0400

    fixed href bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = (props) =>{
     return (
         <React.Fragment>
             <nav className="navbar navbar-custom-style">
-                <a className="navbar-brand navbar-band-style">Medication Visualizer</a>
+                <a href="https://apps.hdap.gatech.edu/medviz2-frontend/" className="navbar-brand navbar-band-style">Medication Visualizer</a>
             </nav>
         </React.Fragment>
     );
