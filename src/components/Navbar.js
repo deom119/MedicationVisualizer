a2056e93085f1795b80a87fb83181ae341ea7331
Dasom <deom119@gmail.com>
2020-03-23 15:29:03 -0400

    nav bar user icon removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,6 @@ const Navbar = (props) =>{
         <React.Fragment>
             <nav className="navbar navbar-custom-style">
                 <a className="navbar-brand navbar-band-style">Medication Visualizer</a>
-                <ul className="nav navbar-nav navbar-right">
-                    <li className="nav-item dropdown">
-                        <a className="nav-link" href="#" id="navbardrop" data-toggle="dropdown">
-                            <i className="fas fa-user-circle fa-2x"></i>
-                        </a>
-                    </li>
-                </ul>
             </nav>
         </React.Fragment>
     );
