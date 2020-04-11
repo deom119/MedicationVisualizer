7ce43363017e76b74866e611083c3764f0fdc3be
Bag, Uday <ubag3@gatech.edu>
2020-04-11 14:33:39 -0400

    Create Footer.js
    Added Footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer-margin">
+            <footer className="footer-distributed">
+                <div className="footer-left">
+                    <div>MedicationVisualizer &copy; 2020</div>
+                </div>
+
+                <div className="footer-right">
+                    <a href="https://github.gatech.edu/gt-cs6440-hit-spring2020/MedicationVisualizer-2">Follow us: <i class="fab fa-github"></i></a>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
