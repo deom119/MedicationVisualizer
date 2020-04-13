b75c5358a41458b475b56db727446ea82781ece1
Dasom <deom119@gmail.com>
2020-04-13 17:36:39 +0900

    explanation change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                     <div className="banner-content">
                         <div className="pos"><img src={header} alt="" className="image-size"/></div>
                         <div className="tag"><h1 className="text-uppercase">A center of <br></br>Rx visualization </h1></div>                                           
-                        <div className="tag-line"><p>Medication Visualizer is an intuitive web-based visualization application, which aims to provide healthcare professionals with a visual representation of all available medications to make it easier to sort through the appropriate products with more meaningful groupings.</p></div>                        
+                            <div className="tag-line"><p><b>Medication Visualizer</b> is an intuitive web-based visualization application, which aims to provide healthcare professionals with a visual representation of all available medications to make it easier to sort through the appropriate products with more meaningful groupings.</p></div>
                     </div>
             </div>
 
@@ -21,9 +21,9 @@ const Home = () => {
                         <div className="icon"><i class="fas fa-search-location"></i></div>
                         <div>
                             <h4 className="header">Search</h4>
+                            <h6 className="header">Search medications with dropdown and display results in a table</h6>
                             <p className="detail">Users can search the medication database by medication name</p>
                             <p className="detail">Users can search the medication database by medication ID</p>
-                            <p className="detail">Users can search the medication database by medication code system</p>
                         </div>
                     </div>
   
@@ -31,7 +31,7 @@ const Home = () => {
                         <div className="icon"><i class="far fa-chart-bar"></i></div>
                         <div>
                             <h4 className="header">Dashboard</h4>
-                            <p className="detail">Important aspect of medications visualized as charts</p>
+                            <h6 className="header">Important aspect of medications visualized as charts</h6>
                             <p className="detail">Bar graph displaying the number of medications expiring for a certain year</p>
                             <p className="detail">Doughnut graph displaying the quantity of medications that belong to a particular batch</p>
                         </div>
@@ -41,9 +41,9 @@ const Home = () => {
                         <div className="icon"><i class="fas fa-search-plus"></i></div>
                         <div>
                             <h4 className="header">Search</h4>
+                            <p className="detail">Users can search the medication database by medication code system</p>
                             <p className="detail">Users can search the medication database by medication code</p>
                             <p className="detail">Users can search the medication database by medication ingredient</p>
-                            <p className="detail">Users can search the medication database by medication form</p>
                         </div>
                     </div>
                     
@@ -52,8 +52,8 @@ const Home = () => {
                         <div>
                             <h4 className="header">Dashboard</h4>
                             <p className="detail">Doughnut graph of the ingredients of all medications</p>
+                            <p className="detail">Bar graph of the number of ingredients that make up a medication</p>
                             <p className="detail">Pie chart of the quantity of medications for each medication form</p>
-                            <p className="detail">Pie chart of the number of medications to belong to each medication code system</p>
                         </div>
                     </div>
 
@@ -61,6 +61,7 @@ const Home = () => {
                         <div className="icon"><i class="fas fa-search"></i></div>
                         <div>
                             <h4 className="header">Search</h4>
+                            <p className="detail">Users can search the medication database by medication form</p>
                             <p className="detail">Users can search the medication database by medication expiration year</p>
                             <p className="detail">Users can search the medication database by medication batch lot number </p>
                         </div>
@@ -69,7 +70,8 @@ const Home = () => {
                     <div className="item">
                         <div className="icon"><i class="fas fa-chart-line"></i></div>
                         <div>
-                            <h4 className="header">Dashboard</h4>                            
+                            <h4 className="header">Dashboard</h4>
+                            <p className="detail">Pie chart of the number of medications to belong to each medication code system</p>
                             <p className="detail">Doughnut chart of the status for active medications</p>
                             <p className="detail">Doughnut chart of the medications that were dispensed and the quantities in which dispensed</p>
                             
